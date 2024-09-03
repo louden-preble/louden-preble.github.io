149346f056519f4891f4c116ec87a2bb7b0c9bb1
louden-preble <124315802+louden-preble@users.noreply.github.com>
2024-09-03 16:40:22 -0500

    lelel

--- SNAIL-IDE/static/redirect.js
+++
@@ -1,4 +0,0 @@
-if (window.location.href.includes("snail-ide.js.org")) {
-    const currentPath = window.location.pathname;
-    window.location.href = `https://editor.snail-ide.com${currentPath}`;
-}
