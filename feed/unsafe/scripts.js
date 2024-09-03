b7ac4946e21eba1883cc170aaa9f8aa2a9cfa5a3
louden-preble <124315802+louden-preble@users.noreply.github.com>
2024-09-02 23:57:39 -0500

    Update scripts.js

--- feed/unsafe/scripts.js
+++ feed/unsafe/scripts.js
@@ -1,13 +1,13 @@
 // scripts.js
-const feedUrls = [
-    "https://www.smbc-comics.com/comic/rss",
-    "https://xkcd.com/rss.xml",
-    "http://feeds.feedburner.com/buttersafe",
-    "https://pbfcomics.com/comics/rss",
-    "https://www.jspowerhour.com/comics.rss"
-];
+const feeds = {
+    smbc: "https://www.smbc-comics.com/comic/rss",
+    xkcd: "https://xkcd.com/rss.xml",
+    buttersafe: "http://feeds.feedburner.com/buttersafe",
+    pbf: "https://pbfcomics.com/comics/rss",
+    jsph: "https://www.jspowerhour.com/comics.rss"
+};
 
-async function fetchRSSFeed(url) {
+async function fetchRSSFeed(url, feedId) {
     const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`);
     const data = await response.json();
     const parser = new DOMParser();
@@ -22,19 +22,25 @@ async function fetchRSSFeed(url) {
         
         html += `
             <div class="feed-item">
-                <h2><a href="${link}" target="_blank">${title}</a></h2>
+                <h3><a href="${link}" target="_blank">${title}</a></h3>
                 <p>${description}</p>
             </div>
         `;
     });
 
-    document.getElementById("feed").innerHTML += html;
+    document.getElementById(`${feedId}-feed`).innerHTML = html;
 }
 
 async function loadFeeds() {
-    for (const url of feedUrls) {
-        await fetchRSSFeed(url);
+    for (const [feedId, url] of Object.entries(feeds)) {
+        await fetchRSSFeed(url, feedId);
     }
+}
+
+function jumpToFeed() {
+    const select = document.getElementById("feed-select");
+    const feedId = select.value;
+    document.getElementById(feedId).scrollIntoView({ behavior: "smooth" });
 }
 
 loadFeeds();
